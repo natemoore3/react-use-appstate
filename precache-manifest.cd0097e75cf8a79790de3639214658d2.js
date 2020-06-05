bcef4163e28afea83943fdacf50e2f667f156ba1
Nathan Moore <natemoore3@gmail.com>
2020-06-05 11:17:57 -0500

    Updates

---
+++ precache-manifest.cd0097e75cf8a79790de3639214658d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "727aad4ccdd472353c37916b19f1fb25",
+    "url": "/react-use-appstate/index.html"
+  },
+  {
+    "revision": "9bc91cb13b6447c6cb18",
+    "url": "/react-use-appstate/static/js/2.8e86ef27.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-use-appstate/static/js/2.8e86ef27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef399c60b1da18fe3206",
+    "url": "/react-use-appstate/static/js/main.71f863f9.chunk.js"
+  },
+  {
+    "revision": "d4520969e0c79e608f1e",
+    "url": "/react-use-appstate/static/js/runtime-main.c13a8251.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-use-appstate/precache-manifest.80a689f920fec7a910a1a78df0562553.js"
+  "/react-use-appstate/precache-manifest.cd0097e75cf8a79790de3639214658d2.js"
 );
 
 self.addEventListener('message', (event) => {
